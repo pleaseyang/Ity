da1bcda27a91e6567109943a4c2448d97e1307ef
杨洋 <tjbdyangyang@163.com>
2021-01-05 09:56:22 +0800

    delete web index

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,20 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * homePage
-     *
-     * @return \Illuminate\Contracts\View\View
-     */
-    public function home()
-    {
-        View::addExtension('html', 'php');
-        return view()->file(public_path('index.html'));
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [Controller::class, 'home']);
