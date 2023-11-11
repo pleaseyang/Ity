7da482213f43e29f2568b7bdd27f2f4703edd9f6
杨洋 <tjbdyangyang@163.com>
2023-11-11 23:46:23 +0800

    update to laravel10

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('cache:prune-stale-tags')->hourly();
         // $schedule->command('inspire')->hourly();
     }
 
